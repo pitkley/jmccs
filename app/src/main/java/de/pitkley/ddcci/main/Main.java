2fa5e53c95a59dd7e02500e828303473f43fac12
Pit Kleyersburg <pitkley@googlemail.com>
2014-11-10 20:03:52 +0100

    Added native parts for OS X, tests in Main class

--- app/src/main/java/de/pitkley/ddcci/main/Main.java
+++ app/src/main/java/de/pitkley/ddcci/main/Main.java
@@ -1,11 +1,16 @@
 package de.pitkley.ddcci.main;
 
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
 import de.pitkley.ddcci.monitor.*;
 
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static void testMonitorManager() {
         msg("Getting monitor manager");
         MonitorManager monitorManager = Monitors.getMonitorManager();
         msg("Getting monitors");
@@ -33,6 +38,38 @@ public class Main {
         }
         msg("Closing monitors");
         monitorManager.closeMonitors();
+    }
+
+    private static void testLibOSX() {
+        int displayCount = LibOSX.INSTANCE.getDisplayCount(5);
+        System.out.println("displayCount: " + displayCount);
+
+        Pointer onlineDisplays = new Memory(5 * Native.getNativeSize(Integer.TYPE));
+        IntByReference ptrDisplayCount = new IntByReference();
+        LibOSX.INSTANCE.getOnlineDisplayList(5, onlineDisplays, ptrDisplayCount);
+        System.out.println(onlineDisplays);
+        System.out.println(ptrDisplayCount.getValue());
+        for (int i = 0; i < ptrDisplayCount.getValue(); i++) {
+            System.out.println(onlineDisplays.getInt(i * Native.getNativeSize(Integer.TYPE)));
+        }
+    }
+
+    private static void testCoreGraphics() {
+        int maxDisplays = 5;
+
+        Pointer onlineDisplays = new Memory(maxDisplays * Native.getNativeSize(Integer.TYPE));
+        IntByReference ptrDisplayCount = new IntByReference();
+
+        CoreGraphics.INSTANCE.CGGetOnlineDisplayList(maxDisplays, onlineDisplays, ptrDisplayCount);
+        msg("onlineDisplays: "+onlineDisplays);
+        msg("displayCount: "+ptrDisplayCount.getValue());
+        for (int i = 0; i < ptrDisplayCount.getValue(); i++) {
+            msg("Display " + i + ": " + onlineDisplays.getInt(i * Native.getNativeSize(Integer.TYPE)));
+        }
+    }
+
+    public static void main(String[] args) {
+        testCoreGraphics();
     }
 
     private static long lastMsg = 0L;
