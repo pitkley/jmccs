e58991f1e4a10320fcf3bda7c56f61f45deb3490
Pit Kleyersburg <pitkley@googlemail.com>
2015-06-21 20:57:22 +0200

    Removed `Monitors`-class (closes issue #3)

--- monitor/src/main/java/de/pitkley/jmccs/monitor/MonitorManager.java
+++ monitor/src/main/java/de/pitkley/jmccs/monitor/MonitorManager.java
@@ -1,21 +1,54 @@
 package de.pitkley.jmccs.monitor;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-public interface MonitorManager {
+public abstract class MonitorManager {
+    private static MonitorManager INSTANCE = null;
 
-    List<Monitor> getMonitors();
+    public static MonitorManager get() {
+        if (INSTANCE == null) {
+            loadManagers();
+        }
+        return INSTANCE;
+    }
+
+    private static void loadManagers() {
+        Iterator<MonitorManager> managers = ServiceLoader.load(MonitorManager.class).iterator();
+        MonitorManager manager = null;
+        //noinspection WhileLoopReplaceableByForEach
+        while (managers.hasNext()) {
+            try {
+                MonitorManager curManager = managers.next();
+                manager = curManager;
+                break;
+            } catch (ServiceConfigurationError e) {
+                if (!(e.getCause() instanceof UnsupportedOperatingSystemException)) {
+                    throw e;
+                }
+            }
+        }
+
+        if (manager == null) {
+            // TODO Throw appropriate error if we found no manager
+            // For now, we just stop with an error
+            throw new Error("no manager found");
+        } else {
+            INSTANCE = manager;
+        }
+    }
+
+    public abstract List<Monitor> getMonitors();
 
     /**
      * Tries to get the main/primary monitor.
      * This should maybe return a list, as it is bound to HMONITOR under Windows which may not correlate to a
      * physical display.
+     *
      * @return
      */
-    default Optional<Monitor> getMainMonitor() {
+    public Optional<Monitor> getMainMonitor() {
         return getMonitors().stream().filter(Monitor::isMainMonitor).findFirst();
     }
 
-    void closeMonitors();
+    public abstract void closeMonitors();
 }
