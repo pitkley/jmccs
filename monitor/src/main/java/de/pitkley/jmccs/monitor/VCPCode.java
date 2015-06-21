8dfdf896b4196d9f4c624fa9d7cc5ad5d524fa46
Pit Kleyersburg <pitkley@googlemail.com>
2015-06-21 14:10:34 +0200

    Added `isLegal` to `VCPCode`

--- monitor/src/main/java/de/pitkley/jmccs/monitor/VCPCode.java
+++ monitor/src/main/java/de/pitkley/jmccs/monitor/VCPCode.java
@@ -1,23 +1,57 @@
 package de.pitkley.jmccs.monitor;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public enum VCPCode {
     /**
-     * <p>Restore all factory presets including luminance / contrast, geometry, color and TV defaults.</p>
+     * Restore all factory presets including luminance / contrast, geometry, color and TV defaults.
      * <p>
-     * <p>Any non-zero value causes defaults to be restored. A value of zero must be ignored.</p>
+     * Any non-zero value causes defaults to be restored. A value of zero must be ignored.
      */
-    RESTORE_FACTORY_DEFAULTS(0x04, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS),
+    RESTORE_FACTORY_DEFAULTS(0x04, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, Helper.POSITIVE_TWO_BYTES),
     /**
-     * <p>Restore factory defaults for luminance and contrast adjustments.</p>
+     * Restore factory defaults for luminance and contrast adjustments.
      * <p>
-     * <p>Any non-zero value causes defaults to be restored. A value of zero must be ignored.</p>
+     * Any non-zero value causes defaults to be restored. A value of zero must be ignored.
      */
-    RESTORE_FACTORY_LUMINANCE_CONTRAST_DEFAULTS(0x05, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS),
+    RESTORE_FACTORY_LUMINANCE_CONTRAST_DEFAULTS(0x05, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, Helper.POSITIVE_TWO_BYTES),
     /**
-     * <p>Permits the selection of a preset optimized by manufacturer for an application type or the selection of
-     * a user defined setting.</p>
+     * Restore factory defaults for geometry adjustments.
+     * <p>
+     * Any non-zero value causes defaults to be restored. A value of zero must be ignored.
+     */
+    RESTORE_FACTORY_GEOMETRY_DEFAULTS(0x06, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, Helper.POSITIVE_TWO_BYTES),
+    /**
+     * Restore factory defaults for color settings.
+     * <p>
+     * Any non-zero value causes defaults to be restored. A value of zero must be ignored.
+     */
+    RESTORE_FACTORY_COLOR_DEFAULTS(0x08, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, Helper.POSITIVE_TWO_BYTES),
+    /**
+     * Restore factory defaults for TV functions.
+     * <p>
+     * Any non-zero value causes defaults to be restored. A value of zero must be ignored.
+     */
+    RESTORE_FACTORY_TV_DEFAULTS(0x0A, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, Helper.POSITIVE_TWO_BYTES),
+    /**
+     * Store / Restore the user saved values for current mode.
+     * <p>
+     * <table>
+     * <thead><tr><th>Byte: SL</th><th>Description</th></tr></thead>
+     * <tbody>
+     * <tr><td>01h</td><td>Store current settings in the monitor.</td></tr>
+     * <tr><td>02h</td><td>Restore factory defaults for current mode. If no factory defaults then restore user values for current mode.</td></tr>
+     * </tbody>
+     * </table>
+     * <p>
+     * All other values are reserved and must be ignored.
+     */
+    SETTINGS(0xB0, VCPCodeType.WRITE_ONLY, VCPCodeFunction.NON_CONTINOUS, new int[]{0x01, 0x02}),
+    /**
+     * Permits the selection of a preset optimized by manufacturer for an application type or the selection of
+     * a user defined setting.
      * <p>
      * <table>
      * <thead>
@@ -36,6 +70,7 @@ public enum VCPCode {
      */
     LUMINANCE(0x10, VCPCodeType.READ_WRITE, VCPCodeFunction.CONTINOUS);
 
+
     private static final Map<Integer, VCPCode> mapCodeToVCPCode = new HashMap<>();
 
     static {
@@ -47,13 +82,10 @@ public enum VCPCode {
     private final int code;
     private final VCPCodeType codeType;
     private final VCPCodeFunction codeFunction;
-    private final Optional<Set<Integer>> values;
+    private final Predicate<Integer> isLegal;
 
     VCPCode(int code, VCPCodeType codeType, VCPCodeFunction codeFunction) {
-        this.code = code;
-        this.codeType = codeType;
-        this.codeFunction = codeFunction;
-        this.values = Optional.empty();
+        this(code, codeType, codeFunction, (i -> true));
     }
 
     VCPCode(int code, VCPCodeType codeType, VCPCodeFunction codeFunction, int[] values) {
@@ -62,11 +94,17 @@ public enum VCPCode {
         this.codeFunction = codeFunction;
 
         Set<Integer> v = new HashSet<>();
-        this.values = Optional.of(v);
-
         for (int value : values) {
             v.add(value);
         }
+        this.isLegal = Helper.MAP_VALUES.apply(v);
+    }
+
+    VCPCode(int code, VCPCodeType codeType, VCPCodeFunction codeFunction, Predicate<Integer> isLegal) {
+        this.code = code;
+        this.codeType = codeType;
+        this.codeFunction = codeFunction;
+        this.isLegal = isLegal;
     }
 
     public int getCode() {
@@ -81,11 +119,20 @@ public enum VCPCode {
         return codeFunction;
     }
 
-    public Optional<Set<Integer>> getValues() {
-        return values;
+    public boolean isValueLegal(int value) {
+        return isLegal.test(value);
     }
 
     public static Optional<VCPCode> get(int code) {
         return Optional.ofNullable(mapCodeToVCPCode.get(code));
     }
+}
+
+class Helper {
+    protected static final Predicate<Integer> POSITIVE = (i) -> i >= 0;
+    protected static final Predicate<Integer> TWO_BYTES = (i) -> i < Math.pow(2, 2 * 8);
+
+    protected static final Predicate<Integer> POSITIVE_TWO_BYTES = POSITIVE.and(TWO_BYTES);
+
+    protected static final Function<Set<Integer>, Predicate<Integer>> MAP_VALUES = (v) -> v::contains;
 }
