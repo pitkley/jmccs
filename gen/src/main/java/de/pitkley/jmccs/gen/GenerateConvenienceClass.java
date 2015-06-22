12bf2ead00dbb5d729cdfe866cc616d164676579
Pit Kleyersburg <pitkley@googlemail.com>
2015-06-22 18:09:31 +0200

    Moved `jmccs-gen` modules to main project
    Closes issue #4.

---
+++ gen/src/main/java/de/pitkley/jmccs/gen/GenerateConvenienceClass.java
@@ -0,0 +1,150 @@
+package de.pitkley.jmccs.gen;
+
+import com.google.common.io.CharStreams;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import de.pitkley.jmccs.monitor.Monitor;
+import de.pitkley.jmccs.monitor.VCPCode;
+import de.pitkley.jmccs.monitor.VCPCodeType;
+import de.pitkley.jmccs.monitor.VCPReply;
+import org.json.JSONArray;
+
+import javax.lang.model.element.Modifier;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+
+public class GenerateConvenienceClass {
+
+    private static void generateMonitorHelper(Path path) throws IOException, URISyntaxException {
+        TypeSpec.Builder monitorHelperBuilder = TypeSpec.classBuilder("MonitorHelper")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(AnnotationSpec.builder(SuppressWarnings.class).addMember("value", "$S", "unused").build())
+                .addSuperinterface(Monitor.class)
+                .addField(Monitor.class, "monitor", Modifier.PRIVATE, Modifier.FINAL);
+
+        MethodSpec constructor = MethodSpec.constructorBuilder()
+                .addParameter(Monitor.class, "monitor")
+                .addStatement("this.$L = $L", "monitor", "monitor")
+                .addModifiers(Modifier.PUBLIC)
+                .build();
+        monitorHelperBuilder.addMethod(constructor);
+
+        List<MethodSpec> interfaceMethods = generateInterfaceMethods();
+        interfaceMethods.forEach(monitorHelperBuilder::addMethod);
+
+        InputStream vcpStream = GenerateConvenienceClass.class.getClassLoader().getResourceAsStream("vcp.json");
+        try (InputStreamReader isr = new InputStreamReader(vcpStream);
+             BufferedReader br = new BufferedReader(isr)) {
+            String vcpJsonString = CharStreams.toString(br);
+            JSONArray vcpJson = new JSONArray(vcpJsonString);
+            IntStream.range(0, vcpJson.length()).mapToObj(vcpJson::getJSONObject).forEach((j -> {
+                String vcpCodeName = j.getString("vcp_code_name");
+                String transformedName = transformVCPCodeName(vcpCodeName);
+
+                VCPCode c = VCPCode.valueOf(GenerateEnumEntries.transformVCPCodeName(vcpCodeName));
+                List<MethodSpec> methods = generateMethodsForVCPCode(transformedName, c);
+                methods.forEach(monitorHelperBuilder::addMethod);
+            }));
+        }
+
+        JavaFile javaFile = JavaFile.builder("de.pitkley.jmccs.monitor", monitorHelperBuilder.build()).build();
+        javaFile.writeTo(path);
+    }
+
+    private static List<MethodSpec> generateInterfaceMethods() {
+        List<MethodSpec> methods = new ArrayList<>();
+
+        methods.add(MethodSpec.methodBuilder("isMainMonitor")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(boolean.class)
+                .addStatement("return $L.$L()", "monitor", "isMainMonitor")
+                .build());
+        methods.add(MethodSpec.methodBuilder("isVCPCodeSupported")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(boolean.class)
+                .addParameter(VCPCode.class, "vcpCode")
+                .addStatement("return $L.$L($L)", "monitor", "isVCPCodeSupported", "vcpCode")
+                .build());
+        methods.add(MethodSpec.methodBuilder("getVCPFeature")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(VCPReply.class)
+                .addParameter(VCPCode.class, "vcpCode")
+                .addStatement("return $L.$L($L)", "monitor", "getVCPFeature", "vcpCode")
+                .build());
+        methods.add(MethodSpec.methodBuilder("setVCPFeature")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(boolean.class)
+                .addParameter(VCPCode.class, "vcpCode")
+                .addParameter(int.class, "value")
+                .addStatement("return $L.$L($L, $L)", "monitor", "setVCPFeature", "vcpCode", "value")
+                .build());
+        methods.add(MethodSpec.methodBuilder("isClosed")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(boolean.class)
+                .addStatement("return $L.$L()", "monitor", "isClosed")
+                .build());
+        methods.add(MethodSpec.methodBuilder("close")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PUBLIC)
+                .addException(IOException.class)
+                .addStatement("$L.$L()", "monitor", "close")
+                .build());
+
+        return methods;
+    }
+
+    private static List<MethodSpec> generateMethodsForVCPCode(String name, VCPCode vcpCode) {
+        List<MethodSpec> methods = new ArrayList<>();
+        if (vcpCode.getCodeType() == VCPCodeType.READ_ONLY || vcpCode.getCodeType() == VCPCodeType.READ_WRITE) {
+            methods.add(MethodSpec.methodBuilder("get" + name)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(VCPReply.class)
+                    .addStatement("return $L.$L($T.$L)", "monitor", "getVCPFeature", vcpCode.getClass(), vcpCode)
+                    .build());
+        }
+        if (vcpCode.getCodeType() == VCPCodeType.WRITE_ONLY || vcpCode.getCodeType() == VCPCodeType.READ_WRITE) {
+            methods.add(MethodSpec.methodBuilder("set" + name)
+                    .addModifiers(Modifier.PUBLIC)
+                    .addParameter(int.class, "value")
+                    .returns(boolean.class)
+                    .addStatement("$T $L = $T.$L", vcpCode.getClass(), "c", vcpCode.getClass(), vcpCode)
+                    .beginControlFlow("if (!$L.$L($L))", "c", "isValueLegal", "value")
+                    .addStatement("throw new $T(\"The value '\" + value + \"' is illegal\")", IllegalStateException.class)
+                    .endControlFlow()
+                    .addStatement("return $L.$L($L, $L)", "monitor", "setVCPFeature", "c", "value")
+                    .build());
+        }
+
+        return methods;
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Missing output-path");
+        }
+        generateMonitorHelper(Paths.get(args[0]));
+    }
+
+    private static String transformVCPCodeName(String vcpCodeName) {
+        return LOWER_UNDERSCORE.to(UPPER_CAMEL,
+                vcpCodeName.trim().replaceAll("[^a-zA-Z0-9\\s]", " ").replaceAll(" +", "_").toLowerCase());
+    }
+}
---
+++ gen/src/main/java/de/pitkley/jmccs/gen/GenerateEnumEntries.java
@@ -0,0 +1,85 @@
+package de.pitkley.jmccs.gen;
+
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GenerateEnumEntries {
+    private static final Map<String, String> CODE_TYPE_MAP = new HashMap<>();
+    private static final Map<String, String> CODE_FUNCTION_MAP = new HashMap<>();
+    private static final Map<String, String> IS_LEGAL_MAP = new HashMap<>();
+
+    static {
+        CODE_TYPE_MAP.put("RO", "VCPCodeType.READ_ONLY");
+        CODE_TYPE_MAP.put("WO", "VCPCodeType.WRITE_ONLY");
+        CODE_TYPE_MAP.put("RW", "VCPCodeType.READ_WRITE");
+
+        CODE_FUNCTION_MAP.put("C", "VCPCodeFunction.CONTINOUS");
+        CODE_FUNCTION_MAP.put("NC", "VCPCodeFunction.NON_CONTINOUS");
+        CODE_FUNCTION_MAP.put("T", "VCPCodeFunction.TABLE");
+
+        IS_LEGAL_MAP.put("POSITIVE", "Helper.POSITIVE");
+        IS_LEGAL_MAP.put("TWO_BYTES", "Helper.TWO_BYTES");
+        IS_LEGAL_MAP.put("POSITIVE_TWO_BYTES", "Helper.POSITIVE_TWO_BYTES");
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        String vcpJsonString = new String(Files.readAllBytes(Paths.get(GenerateEnumEntries.class.getClassLoader().getResource("vcp.json").toURI())));
+        JSONArray vcpJson = new JSONArray(vcpJsonString);
+
+        String enumString = IntStream.range(0, vcpJson.length())
+                .mapToObj(vcpJson::getJSONObject)
+                .map((j -> {
+                    // Build javadoc
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("/**\n");
+
+                    JSONArray description = j.getJSONArray("description");
+                    IntStream.range(0, description.length())
+                            .mapToObj(description::getString)
+                            .forEach((s -> {
+                                s = s.replaceAll("\n", "\n * ");
+                                sb.append(" * ").append(s).append("\n *\n");
+                            }));
+                    sb.append(" */\n");
+
+                    // Start of enum-definition
+                    sb.append(transformVCPCodeName(j.getString("vcp_code_name"))).append("(");
+                    sb.append(transformHexCode(j.getString("code"))).append(", ");
+                    sb.append(CODE_TYPE_MAP.get(j.getString("type"))).append(", ");
+                    sb.append(CODE_FUNCTION_MAP.get(j.getString("function"))).append(", ");
+
+                    // Values/Is-Legal helper
+                    JSONArray values = j.optJSONArray("values");
+                    if (values != null) {
+                        sb.append("new int[]{");
+                        String s = IntStream.range(0, values.length())
+                                .mapToObj(values::getString)
+                                .map(GenerateEnumEntries::transformHexCode)
+                                .collect(Collectors.joining(", "));
+                        sb.append(s).append("}");
+                    } else {
+                        sb.append(IS_LEGAL_MAP.get(j.getString("values")));
+                    }
+                    sb.append(")");
+                    return sb.toString();
+                })).collect(Collectors.joining(",\n"));
+        enumString += ";";
+        System.out.println(enumString);
+    }
+
+    protected static String transformVCPCodeName(String vcpCodeName) {
+        return vcpCodeName.trim().toUpperCase().replaceAll("[^a-zA-Z0-9\\s]", " ").replaceAll(" +", "_");
+    }
+
+    protected static String transformHexCode(String hexCode) {
+        return "0x" + hexCode.replace("h", "");
+    }
+}
